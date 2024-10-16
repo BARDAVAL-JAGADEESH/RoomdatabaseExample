139a03eb81c88fdb3c5f9a04efb2f76ab982c7b6
Bardaval <jagadeeshbardaval78@gmail.com>
2024-10-16 23:07:01 +0530

    first commit

---
+++ app/src/main/java/com/bardaval/roomdatabaseexample/ContactDAO.kt
@@ -0,0 +1,21 @@
+package com.bardaval.roomdatabaseexample
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ContactDAO {
+
+    @Insert
+    suspend fun insert(contact: Contact)
+
+    @Update
+    suspend fun update(contact: Contact)
+
+    @Delete
+    suspend fun delete(contact: Contact)
+
+    @Query("SELECT * FROM contact")
+    fun getContact() : LiveData<List<Contact>>
+}
